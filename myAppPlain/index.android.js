09d9499fb33f361d55327d68c6e63537b143e6ff
LeadShuriken <d.s.mishev@gmail.com>
2017-09-10 17:56:36 +0300

    Fixed android and ios build with maps and tracking.

--- myAppPlain/index.android.js
+++ myAppPlain/index.android.js
@@ -1,47 +1,164 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
+'use strict';
 
-import React, { Component } from 'react';
+import React, {
+  Component
+} from 'react';
+
 import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  Dimensions
 } from 'react-native';
 
+import MapView from 'react-native-maps';
+
+const {
+  width,
+  height
+} = Dimensions.get('window')
+
+const SCREEN_HEGHT = height;
+const SCREEN_WIDTH = width;
+const ASPECT_RATIO = width / height;
+
+const LATITUDE_DELTA = 0.0922;
+const LONGITUDE_DELTA = 0.0421;
+
 export default class myApp extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      initialPosition: {
+        latitude: 42.698334,
+        longitude: 23.319941,
+        latitudeDelta: LATITUDE_DELTA,
+        longitudeDelta: LONGITUDE_DELTA,
+      },
+      markerPosition: {
+        latitude: 42.698334,
+        longitude: 23.319941,
+      }
+    }
+  }
+
+  watchID: ? number = null
+
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition((position) => {
+        var lat = parseFloat(position.coords.latitude)
+        var long = parseFloat(position.coords.longitude)
+
+        var initialRegion = {
+          latitude: lat,
+          longitude: long,
+          latitudeDelta: LATITUDE_DELTA,
+          longitudeDelta: LONGITUDE_DELTA,
+        }
+
+        this.setState({
+          initialPosition: initialRegion,
+          markerPosition: initialRegion
+        })
+      },
+      (error) => alert(JSON.stringify(error)), {
+        enableHighAccuracy: true,
+        timeout: 20000,
+        maximumAge: 10000
+    });
+
+    this.watchID = navigator.geolocation.watchPosition((position) => {
+      var lat = parseFloat(position.coords.latitude)
+      var long = parseFloat(position.coords.longitude)
+
+      var lastRegion = {
+        latitude: lat,
+        longitude: long,
+        latitudeDelta: LATITUDE_DELTA,
+        longitudeDelta: LONGITUDE_DELTA,
+      }
+
+      this.setState({
+        initialPosition: lastRegion,
+        markerPosition: lastRegion
+      })
+    });
+  }
+
+  componentWillUnmount() {
+    navigator.geolocation.clearWatch(this.watchID)
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
+      <View style = {styles.container} >
+        <MapView style = {styles.map}
+                  initialRegion = {this.state.initialPosition} >
+          <MapView.Marker coordinate = {this.state.markerPosition} >
+            <View style = {styles.radius} >
+              <View style = {styles.marker} ></View>
+            </View>
+          </MapView.Marker>
+        </MapView>
+        <View style = {styles.textContainer} >
+          <Text style = {styles.welcome} >
+            Welcome ThisIS Wild!
+          </Text>
+          <Text style = {styles.instructions} >
+            Work and edit the index.ios.js
+          </Text>
+          <Text style = {styles.instructions} >
+            Press Cmd + R to reload, {'\n'}
+            Cmd + D or shake for dev menu
+          </Text>
+        </View>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
+  radius: {
+    height: 50,
+    width: 50,
+    borderRadius: 25,
+    overflow: 'hidden',
+    backgroundColor: 'rgba(0, 122, 255, 0.1)',
+    borderColor: 'rgba(0, 122, 255, 0.1)',
+    borderWidth: 1,
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    justifyContent: 'center'
+  },
+  marker: {
+    height: 20,
+    width: 20,
+    borderWidth: 3,
+    borderColor: 'white',
+    borderRadius: 10,
+    overflow: 'hidden',
+    backgroundColor: '#007AFF'
+  },
+  textContainer: {
+    top: 30,
+    width: '100%',
+    position: 'absolute',
+    marginLeft: 'auto',
+    backgroundColor: 'transparent'
+  },
+  container:{
+    ...StyleSheet.absoluteFillObject,
+   justifyContent: 'flex-end',
+   alignItems: 'center',
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
   },
   welcome: {
     fontSize: 20,
+    fontWeight: '900',
     textAlign: 'center',
-    margin: 10,
+    margin: 10
   },
   instructions: {
     textAlign: 'center',
--- myAppPlain/index.ios.js
+++ myAppPlain/index.ios.js
@@ -1,11 +1,8 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
+'use strict';
 
-import React, { Component } from 'react';
-import MapView from 'react-native-maps';
+import React, {
+  Component
+} from 'react';
 
 import {
   AppRegistry,
@@ -15,142 +12,147 @@ import {
   Dimensions
 } from 'react-native';
 
-const {width, height} = Dimensions.get('window')
+import MapView from 'react-native-maps';
 
-const SCREEN_HEGHT = height
-const SCREEN_WIDTH = width
-const ASPECT_RATIO = width/height
+const {
+  width,
+  height
+} = Dimensions.get('window')
 
-const LATITUDE_DELTA = 0.0922
-const LONGITUDE_DELTA = 0.0421
+const SCREEN_HEGHT = height;
+const SCREEN_WIDTH = width;
+const ASPECT_RATIO = width / height;
+
+const LATITUDE_DELTA = 0.0922;
+const LONGITUDE_DELTA = 0.0421;
 
 export default class myApp extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      initialPosition: {
+        latitude: 42.698334,
+        longitude: 23.319941,
+        latitudeDelta: LATITUDE_DELTA,
+        longitudeDelta: LONGITUDE_DELTA,
+      },
+      markerPosition: {
+        latitude: 42.698334,
+        longitude: 23.319941,
+      }
+    }
+  }
 
-	constructor(props){
-		super(props)
-		this.state = {
-			initialPosition: {
-				latitude: 0,
-	           	longitude: 0,
-		    	latitudeDelta: LATITUDE_DELTA,
-	      		longitudeDelta: LONGITUDE_DELTA,
-			},
-			markerPosition: {
-				latitude: 0,
-	           	longitude: 0,
-			}
-		}
-	}
+  watchID: ? number = null
 
-	watchID: ?number = null
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition((position) => {
+        var lat = parseFloat(position.coords.latitude)
+        var long = parseFloat(position.coords.longitude)
 
-	componentDidMount(){
-		navigator.geolocation.getCurrentPosition((position) => {
-			var lat = parseFloat(position.coords.latitude)
-			var long = parseFloat(position.coords.longitude)
+        var initialRegion = {
+          latitude: lat,
+          longitude: long,
+          latitudeDelta: LATITUDE_DELTA,
+          longitudeDelta: LONGITUDE_DELTA,
+        }
 
-			var initialRegion =  {
-	    		latitude: lat,
-	           	longitude: long,
-		    	latitudeDelta: LATITUDE_DELTA,
-	      		longitudeDelta: LONGITUDE_DELTA,
-		    }
+        this.setState({
+          initialPosition: initialRegion,
+          markerPosition: initialRegion
+        })
+      },
+      (error) => alert(JSON.stringify(error)), {
+        enableHighAccuracy: true,
+        timeout: 20000,
+        maximumAge: 10000
+    });
 
-		    this.setState({initialPosition: initialRegion})
-		    this.setState({markerPosition: initialRegion})
-		},
-		(error) => alert(JSON.stringify(error)),
-		{enableHighAccuracy: true, timeout: 20000, maximumAge: 10000})
+    this.watchID = navigator.geolocation.watchPosition((position) => {
+      var lat = parseFloat(position.coords.latitude)
+      var long = parseFloat(position.coords.longitude)
 
-		this.watchID = navigator.geolocation.watchPosition((position) => {
-			var lat = parseFloat(position.coords.latitude)
-			var long = parseFloat(position.coords.longitude)
+      var lastRegion = {
+        latitude: lat,
+        longitude: long,
+        latitudeDelta: LATITUDE_DELTA,
+        longitudeDelta: LONGITUDE_DELTA,
+      }
 
-			var lastRegion =  {
-	    		latitude: lat,
-	           	longitude: long,
-		    	latitudeDelta: LATITUDE_DELTA,
-	      		longitudeDelta: LONGITUDE_DELTA,
-		    }
+      this.setState({
+        initialPosition: lastRegion,
+        markerPosition: lastRegion
+      })
+    });
+  }
 
-		    this.setState({initialPosition: lastRegion})
-		    this.setState({markerPosition: lastRegion})
-		})
-	}
+  componentWillUnmount() {
+    navigator.geolocation.clearWatch(this.watchID)
+  }
 
-	componentWillUnmount(){
-		navigator.geolocation.clearWatch(this.watchID)
-	}
-
-  	render() {
-	    return (
-	      <View style={styles.container}>
-	        <MapView
-		         style={styles.map}
-		         initialRegion={this.initialPosition}
-	        >
-		        <MapView.Marker coordinate={this.markerPosition}>
-			        <View style={styles.radius}>
-				        <View style={styles.marker}>
-				        </View>
-			        </View>
-		        </MapView.Marker>
-	        </MapView>
-	        <View style={styles.textContainer}>
-		        <Text style={styles.welcome}>
-		          Welcome ThisIS Wild!
-		        </Text>
-		        <Text style={styles.instructions}>
-		          Work and edit the index.ios.js
-		        </Text>
-		        <Text style={styles.instructions}>
-		          Press Cmd+R to reload,{'\n'}
-		          Cmd+D or shake for dev menu
-		        </Text>
-	        </View>
-	      </View>
-	    );
-	  }
+  render() {
+    return (
+      <View style = {styles.container} >
+        <MapView style = {styles.map}
+                  initialRegion = {this.state.initialPosition} >
+          <MapView.Marker coordinate = {this.state.markerPosition} >
+            <View style = {styles.radius} >
+              <View style = {styles.marker} ></View>
+            </View>
+          </MapView.Marker>
+        </MapView>
+        <View style = {styles.textContainer} >
+          <Text style = {styles.welcome} >
+            Welcome ThisIS Wild!
+          </Text>
+          <Text style = {styles.instructions} >
+            Work and edit the index.ios.js
+          </Text>
+          <Text style = {styles.instructions} >
+            Press Cmd + R to reload, {'\n'}
+            Cmd + D or shake for dev menu
+          </Text>
+        </View>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   radius: {
-  	position: 'absolute',
-  	left: 10,
-  	top: 10,
-  	height: 50,
-  	width: 50,
-  	borderRadius: 25,
-  	overflow: 'hidden',
-  	backgroundColor: 'rgba(0, 122, 255, 0.1)',
-  	borderColor: 'rgba(0, 122, 255, 0.1)',
-  	borderWidth: 1,
-  	alignItems: 'center',
-  	justifyContent: 'center'
+    height: 50,
+    width: 50,
+    borderRadius: 25,
+    overflow: 'hidden',
+    backgroundColor: 'rgba(0, 122, 255, 0.1)',
+    borderColor: 'rgba(0, 122, 255, 0.1)',
+    borderWidth: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
   },
   marker: {
-  	height: 20,
-  	width: 20,
-  	borderWidth: 3,
-  	borderColor: 'white',
-  	borderRadius: 10,
-  	overflow: 'hidden',
-  	backgroundColor: '#007AFF'
+    height: 20,
+    width: 20,
+    borderWidth: 3,
+    borderColor: 'white',
+    borderRadius: 10,
+    overflow: 'hidden',
+    backgroundColor: '#007AFF'
   },
   textContainer: {
-  	top: 30,
-  	position: 'relative',
-  	backgroundColor: 'transparent'
+    top: 30,
+    width: '100%',
+    position: 'absolute',
+    marginLeft: 'auto',
+    backgroundColor: 'transparent'
   },
-  container: {
-    flex: 1
+  container:{
+    ...StyleSheet.absoluteFillObject,
+   justifyContent: 'flex-end',
+   alignItems: 'center',
   },
   map: {
-  	left: 0,
-  	right: 0,
-  	top: 0,
-  	bottom: 0,
-  	position: 'absolute'
+    ...StyleSheet.absoluteFillObject,
   },
   welcome: {
     fontSize: 20,
